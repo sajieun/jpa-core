058cef5b4c92e07cacfa254f9dda3bcd173d9ce8
sajieun <sajieun000721@naver.com>
2025-01-13 14:45:00 +0900

    영속성 컨텍스트의 기능

--- src/main/java/org/sparta/entity/Memo.java
+++ src/main/java/org/sparta/entity/Memo.java
@@ -9,12 +9,36 @@ public class Memo {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
     private Long id;
 
-    // nullable: null 허용 여부
-    // unique: 중복 허용 여부 (false 일때 중복 허용)
     @Column(name = "username", nullable = false, unique = true)
     private String username;
 
-    // length: 컬럼 길이 지정
     @Column(name = "contents", nullable = false, length = 500)
     private String contents;
+
+    // Getter와 Setter 메서드 추가
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
 }
+
+
